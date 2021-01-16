24bbf3f1a6d50c57c6844d9b96b05db87e8dfb40
comedyOfKing <f_hhong@163.com>
2021-01-16 15:12:57 +0800

    修改打包输出路径

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,7 @@
 const { override, addDecoratorsLegacy } = require("customize-cra");
 
-module.exports = override(addDecoratorsLegacy());
+module.exports = override(addDecoratorsLegacy(),config => {
+  // 修改 publicPath 和输出路径
+  config.output.publicPath = './'
+  return config
+});
